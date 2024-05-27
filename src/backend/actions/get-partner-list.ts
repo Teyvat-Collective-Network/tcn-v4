f513113ca01568e6dfe4065f633c2744012dff2b
hyper-neutrino <alex.liao@faire.com>
2024-05-27 10:49:11 -0400

    sort partner list

--- src/backend/actions/get-partner-list.ts
+++ src/backend/actions/get-partner-list.ts
@@ -1,11 +1,13 @@
+import { asc } from "drizzle-orm";
 import { z } from "zod";
 import { db } from "../db/db.js";
-import { proc } from "../trpc.js";
+import tables from "../db/tables.js";
 import trpcify from "../lib/trpcify.js";
 import zs from "../lib/zs.js";
+import { proc } from "../trpc.js";
 
 export default proc.output(z.object({ id: zs.snowflake, mascot: zs.id, name: z.string(), invite: z.string(), image: z.string() }).array()).query(
     trpcify(async () => {
-        return await db.query.guilds.findMany({ columns: { id: true, mascot: true, name: true, invite: true, image: true } });
+        return await db.query.guilds.findMany({ columns: { id: true, mascot: true, name: true, invite: true, image: true }, orderBy: asc(tables.guilds.name) });
     }),
 );
