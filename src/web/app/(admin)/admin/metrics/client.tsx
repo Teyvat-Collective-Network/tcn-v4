017e1fb8f60e688115e2c4f5d2d233a64b89c40a
hyper-neutrino <alex.liao@faire.com>
2024-05-28 10:46:12 -0400

    update metrics page

--- src/web/app/(admin)/admin/metrics/client.tsx
+++ src/web/app/(admin)/admin/metrics/client.tsx
@@ -1,7 +1,72 @@
+"use client";
+
+import { useState } from "react";
+import { FaChevronDown, FaChevronUp, FaMinus } from "react-icons/fa6";
+import { Prose } from "../../../../components/ui/prose";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../../../../components/ui/table";
+
 export default function AdminMetricsClient({ metrics }: { metrics: { key: string; averageDuration: number; count: number; errorRate: number }[] }) {
+    const [sorted, setSorted] = useState<typeof metrics>(metrics);
+    const [sortKey, setSortKey] = useState<"averageDuration" | "count" | "errorRate">("averageDuration");
+    const [sortDir, setSortDir] = useState<boolean>(false);
+
     return (
-        <pre>
-            <code>{JSON.stringify(metrics, null, 4)}</code>
-        </pre>
+        <Prose>
+            <h1>Metrics</h1>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Key</TableHead>
+                        <TableHead>
+                            <div
+                                className="inline-flex items-center gap-2 cursor-default whitespace-nowrap"
+                                onClick={() => (sortKey === "averageDuration" ? setSortDir((x) => !x) : (setSortKey("averageDuration"), setSortDir(false)))}
+                            >
+                                Average Duration
+                                {sortKey === "averageDuration" ? sortDir ? <FaChevronDown /> : <FaChevronUp /> : <FaMinus />}
+                            </div>
+                        </TableHead>
+                        <TableHead>
+                            <div
+                                className="inline-flex items-center gap-2 cursor-default whitespace-nowrap"
+                                onClick={() => (sortKey === "count" ? setSortDir((x) => !x) : (setSortKey("count"), setSortDir(false)))}
+                            >
+                                Count
+                                {sortKey === "count" ? sortDir ? <FaChevronDown /> : <FaChevronUp /> : <FaMinus />}
+                            </div>
+                        </TableHead>
+                        <TableHead>
+                            <div
+                                className="inline-flex items-center gap-2 cursor-default whitespace-nowrap"
+                                onClick={() => (sortKey === "errorRate" ? setSortDir((x) => !x) : (setSortKey("errorRate"), setSortDir(false)))}
+                            >
+                                Error Rate
+                                {sortKey === "errorRate" ? sortDir ? <FaChevronDown /> : <FaChevronUp /> : <FaMinus />}
+                            </div>
+                        </TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {sorted
+                        .sort((x, y) => (sortDir ? x[sortKey] - y[sortKey] : y[sortKey] - x[sortKey]))
+                        .map((row) => (
+                            <TableRow key={row.key}>
+                                <TableCell>
+                                    <code>{row.key}</code>
+                                </TableCell>
+                                <TableCell>
+                                    <code>{row.averageDuration}</code>
+                                </TableCell>
+                                <TableCell>
+                                    <code>{row.count}</code>
+                                </TableCell>
+                                <TableCell>
+                                    <code>{(row.errorRate * 100).toFixed(2)}%</code>{" "}
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                </TableBody>
+            </Table>
+        </Prose>
     );
 }
